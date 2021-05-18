ccf646a2eb8543b6ba3766532ac4fad2a9386219
karimkohel <kareemkohel@gmail.com>
2021-05-19 00:28:17 +0200

    remembered i am working on sender script and loaded the sender code not the receiver

---
+++ overflow.py
@@ -0,0 +1,56 @@
+import socket
+import struct
+import cv2
+import math
+import os
+
+
+class FrameSegment():
+
+    MAX_IMAGE_DGRAM = 2**16 - 64 # minus 64 bytes in case UDP frame overflown
+
+    def __init__(self, port=5000, camNum=0):
+        self.cam = cv2.VideoCapture(camNum)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # for linux use SO_REUSEPORT
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        # self.s.settimeout(0.2)
+        self.PORT = port
+
+    def __del__(self):
+        self.cam.release()
+        self.s.close()
+
+    def send(self):
+        while 1:
+            _, img = self.cam.read()
+            compress_img = cv2.imencode(".jpg", img)[1]
+            dat = compress_img.tostring()
+            size = len(dat)
+            num_of_segments = math.ceil(size/(self.MAX_IMAGE_DGRAM))
+            array_pos_start = 0
+
+            while num_of_segments:
+                array_pos_end = min(size, array_pos_start + self.MAX_IMAGE_DGRAM)
+                self.s.sendto(
+                            struct.pack("B", num_of_segments) +
+                            dat[array_pos_start:array_pos_end], 
+                            ('192.168.2.255', self.PORT)
+                            )
+                array_pos_start = array_pos_end
+                num_of_segments -= 1
+
+
+def isVideo(element):
+    return True if element.startswith("video") else False
+
+def getCams():
+
+    cams = filter(isVideo, os.listdir("/dev/")) # gets list of video devices if isVideo finds them in os list
+    camNums = []
+
+    for cam in cams:
+        res = ''.join(filter(lambda i: i.isdigit(), cam))
+        camNums.append(int(res))
+
+    return camNums
--- sender.py
+++ sender.py
@@ -6,8 +6,7 @@ cam = RovCam(port=5000)
 while True:
 	frame = cam.read()
 
-
-	cv2.imshow("ROV", frame)
+	cv2.imshow("UAV", frame)
 
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
