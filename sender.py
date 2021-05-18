2cd041e118ebc5fc0e002d591010aa94a1478c49
karimkohel <kareemkohel@gmail.com>
2021-05-19 00:26:37 +0200

    updated gitignore and added main files via copy

---
+++ sender.py
@@ -0,0 +1,16 @@
+from cams import RovCam
+import cv2
+
+cam = RovCam(port=5000)
+
+while True:
+	frame = cam.read()
+
+
+	cv2.imshow("ROV", frame)
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cv2.destroyAllWindows()
+cam.s.close()
